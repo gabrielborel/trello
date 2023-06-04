52b48d8081a2aef0038a60434182532fd7b85244
Gabriel Borel <biel_borel@hotmail.com>
2023-06-04 07:42:42 -0300

    feat: methods on columRepo

--- src/domain/repository/ColumnRepository.ts
+++ src/domain/repository/ColumnRepository.ts
@@ -2,4 +2,7 @@ import { Column } from "../entity/Column";
 
 export interface ColumnRepository {
   findAllByBoardId(boardId: number): Promise<Column[]>;
+  save(column: Column): Promise<number>;
+  get(columnId: number): Promise<Column>;
+  delete(columnId: number): Promise<void>;
 }
